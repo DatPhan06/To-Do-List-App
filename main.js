150b919c9537cdcb9e4d133f6095bf44ed421179
Phan Tiến Đạt <126739355+DatPhan06@users.noreply.github.com>
2023-07-09 22:37:55 +0700

    Update main.js

--- main.js
+++ main.js
@@ -10,25 +10,29 @@ function addTodo() {
     event.preventDefault();
     const todoText = todoInput.value.trim();
 
-    if (todoText !== "") {
-        const todoDiv = document.createElement("div");
-        todoDiv.classList.add("todo");
-        const newTodo = document.createElement("li");
-        newTodo.innerText = todoText;
-        newTodo.classList.add("todo-item");
-        todoDiv.appendChild(newTodo);
-        const completedButton = document.createElement("button");
-        completedButton.innerHTML = '<i class="fas fa-check-circle"></i>';
-        completedButton.classList.add("complete-btn");
-        todoDiv.appendChild(completedButton);
-        const trashButton = document.createElement("button");
-        trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-        trashButton.classList.add("trash-btn");
-        todoDiv.appendChild(trashButton);
-        todoList.appendChild(todoDiv);
-        saveLocalTodos(todoText);
-        todoInput.value = "";
+    if (todoText == "") {
+        alert('Please enter a task!');
     }
+        else
+        {
+            const todoDiv = document.createElement("div");
+            todoDiv.classList.add("todo");
+            const newTodo = document.createElement("li");
+            newTodo.innerText = todoText;
+            newTodo.classList.add("todo-item");
+            todoDiv.appendChild(newTodo);
+            const completedButton = document.createElement("button");
+            completedButton.innerHTML = '<i class="fas fa-check-circle"></i>';
+            completedButton.classList.add("complete-btn");
+            todoDiv.appendChild(completedButton);
+            const trashButton = document.createElement("button");
+            trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+            trashButton.classList.add("trash-btn");
+            todoDiv.appendChild(trashButton);
+            todoList.appendChild(todoDiv);
+            saveLocalTodos(todoText);
+            todoInput.value = "";
+        }
 }
 
 // Hàm xử lý sự kiện khi người dùng nhấp vào danh sách công việc
